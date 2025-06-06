5ebfdf4289baf5e883277626061f891b23c18509
maria <mpramos78915400@gmail.com>
2025-06-05 22:58:32 -0400

    primer labo del modulo 2

---
+++ modulo 2/lab1.js
@@ -0,0 +1,16 @@
+let nameContact1= "Maxwell Wright"
+let phoneContact1= "(0191) 719 6495"
+let emailContact1="Curabitur.egestas-nunc@nonummyac.co.uk"
+let nameContact2="Raja Villarreal"
+let phoneContact2="0866398 2895"
+let emailContact2="posuere.vulputate@sed.com"
+let nameContact3="Helen Richards"
+let phoneContact3="0800 1111"
+let emailContact3="libero@convallis.edu"
+
+// console.log(nameContact1 + phoneContact1 + emailContact1)
+//? PRIMERA FORMA
+// console.log(nameContact1 + " / " + phoneContact1 + " / " + emailContact1); /*✔*/
+//? SEGUNDA FORMA
+console.log(`${nameContact1} / ${phoneContact1} / ${emailContact1}`); /*✔*/
+// ALT Gr + }
